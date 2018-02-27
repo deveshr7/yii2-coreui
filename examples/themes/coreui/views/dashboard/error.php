c669ae69613e2ac20c6b0215df5e9d8bb171cb25
Klaus Mergen <klaus.mergen@web.de>
2018-02-27 09:34:49 +0100

    refine error view

--- examples/themes/coreui/views/dashboard/error.php
+++ examples/themes/coreui/views/dashboard/error.php
@@ -2,33 +2,44 @@
 /* @var $this yii\web\View */
 /* @var $name string */
 /* @var $message string */
+
 /* @var $exception Exception */
 
 use yii\helpers\Html;
 
 $this->title = $name;
-//$this->context->layout = 'blank';
+$this->params['bodyCssClass'] = 'app flex-row align-items-center';
 ?>
-<div class="dashboard-error error-page">
-    <?php if ($exception->statusCode < 500): ?>
-        <h2 class="headline text-yellow"><?= $exception->statusCode ?></h2>
-        <div class="error-content">
-            <h3><i class="fa fa-warning text-yellow"></i> Oops! Something went wrong.</h3>
-            <p>
-                <?= nl2br(Html::encode($message)) ?>
-                You may <?= Html::a('return to dashboard', ['/']) ?>.
-            </p>
-        </div>
-    <?php else: ?>
-        <h2 class="headline text-red"><?= $exception->statusCode ?></h2>
-        <div class="error-content">
-            <h3><i class="fa fa-warning text-red"></i> Oops! Something went wrong.</h3>
-
-            <p>
-                <?= nl2br(Html::encode($message)) ?>
-                We will work on fixing that right away.
-                You may <?= Html::a('return to dashboard', ['/']) ?>.
-            </p>
-        </div>
-    <?php endif; ?>
+<div class="error-page container">
+    <div class="row justify-content-center">
+        <?php if ($exception->statusCode < 500): ?>
+            <div class="col-md-6">
+                <div class="clearfix">
+                    <h1 class="float-left display-3 mr-4 text-warning"><?= $exception->statusCode ?></h1>
+                    <div class="error-content">
+                        <h3><i class="fa fa-warning text-warning"></i> Oops! Something went wrong.</h3>
+                        <p>
+                            <?= nl2br(Html::encode($message)) ?>
+                            You may <?= Html::a('return to dashboard', ['/']) ?>.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        <?php else: ?>
+            <div class="col-md-6">
+                <div class="clearfix">
+                    <h1 class="float-left display-3 mr-4 text-danger"><?= $exception->statusCode ?></h1>
+                    <div class="error-content">
+                        <h3><i class="fa fa-warning text-danger"></i> Oops! Something went wrong.</h3>
+                        <p>
+                            <?= nl2br(Html::encode($message)) ?>
+                            We will work on fixing that right away.
+                            You may <?= Html::a('return to dashboard', ['/']) ?>.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
 </div>
+
